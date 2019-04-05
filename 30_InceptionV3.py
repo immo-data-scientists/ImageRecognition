2b973e2098829b48855227ed16c47c72d45fed6e
Jishnu Prakash K P <jishnu@IAZI.LOCAL>
2019-04-05 17:11:04 +0530

    Organised paths to the models.

--- 30_InceptionV3.py
+++ 30_InceptionV3.py
@@ -5,18 +5,33 @@ Created on Wed Apr  3 17:22:02 2019
 @author: parveen
 """
 
-from inception_v3 import InceptionV3
+from os.path import join
+from os.path import dirname
+from keras.models import load_model
 from keras.preprocessing import image
 from imagenet_utils import preprocess_input, decode_predictions
 import numpy as np
 
-model = InceptionV3(weights='imagenet')
+# Meta information
+suffix = '_InceptionV3'
+Service = 'ServiceImage'
+ProjHome = 'T:/AI-Product/_Service'
+ImagePath = join(dirname(ProjHome), 'zz_sampledata')
 
-img_path = 'ball.jfif'
-img = image.load_img(img_path, target_size=(299, 299))
+model = load_model(join(ProjHome, Service + suffix + '.hdf5'))
+
+#model = InceptionV3(weights='imagenet')
+
+
+# load and prepare image
+img = image.load_img(join(ImagePath, 'elephant.jpg'), target_size=(299, 299))
+#image = Image.open(img)
+
+img.show()
 x = image.img_to_array(img)
 x = np.expand_dims(x, axis=0)
 x = preprocess_input(x)
 
+# Make predictions
 preds = model.predict(x)
-print('Predicted:', decode_predictions(preds))
+print('Predicted:', decode_predictions(preds) )
